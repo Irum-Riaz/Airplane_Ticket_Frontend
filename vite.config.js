e883d54144b5d090b7b0f7a984a2a427e8bc04a2
Irum-Riaz <hafizairum786@gmail,com>
2024-12-15 00:04:22 +0500

    update vite version

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     // ... other build options
+//     rollupOptions: {
+//       external: ["@babel/runtime/helpers/*"],
+//     },
+//   },
+// });
+
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      onwarn(warning, defaultHandler) {
+        console.warn(warning); // Log the warning details
+        defaultHandler(warning); // Default handling for the warning
+      },
+    }),
+  ],
   build: {
     // ... other build options
     rollupOptions: {
